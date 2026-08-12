a21793c82a5309cf25539d4ede99859028ba7074
jqnevelyn8888 <jqn.evelyn@gmail.com>
2026-08-12 12:09:35 -0400

    pretty mode testing

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+// pretty toggle
+
+var prettyToggle = document.getElementById("pretty-toggle"); // the checkbox slider
+
+prettyToggle.addEventListener('change', (e) => {
+    var elements = document.querySelectorAll('*');
+    if (e.target.checked) {
+        // html.style.backgroundImage = "url('imgs/Aesthetic-Pastel-Cute-PowerPoint-Background-with-Rainbow-and-Baby-Blue-Clouds-by-SlidesCorner.com_.jpg')";
+        elements.forEach(el => {
+            el.classList.add('pretty-mode');
+        });
+    } else {
+        // html.style.backgroundImage = 'none';
+        elements.forEach(el => {
+            el.classList.remove('pretty-mode');
+        });
+    }
+})
+
+
+
+
+
 function test() {
     alert("Test");
 }
